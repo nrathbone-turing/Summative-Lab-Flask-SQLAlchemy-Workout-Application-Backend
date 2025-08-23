27df67666b872d612a0636963e62bc3cb4067e1f
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-23 13:54:27 -0600

    Refactor seed.py file with example data to confirm db functionality after migration and initialization

--- server/seed.py
+++ server/seed.py
@@ -10,6 +10,7 @@ PSEUDOCODE SEED SCRIPT:
 
 from server.app import app
 from server.models import db, Exercise, Workout
+from datetime import date
 
 with app.app_context():
     # Clear out old data
@@ -22,8 +23,8 @@ with app.app_context():
     running = Exercise(name="Running", category="Cardio")
 
     # Create sample Workouts data
-    workout1 = Workout(date="2025-08-23", duration_minutes=30)
-    workout2 = Workout(date="2025-08-24", duration_minutes=45)
+    workout1 = Workout(date=date(2025, 8, 23), duration_minutes=30)
+    workout2 = Workout(date=date(2025, 8, 24), duration_minutes=45)
 
     # Add to session and commit
     db.session.add_all([pushups, squats, running, workout1, workout2])
